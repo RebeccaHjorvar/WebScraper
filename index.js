6172c0eb06ec2ec73dd2a65b8c8e2b02257eb9b3
Rebecca Hjörvar <rebecca.hjorvarroutlook.com>
2022-01-24 14:12:37 +0100

    initial commit, scrape returning empty string

---
+++ index.js
@@ -0,0 +1,24 @@
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+
+const url = 'https://espressohouse.com/produkter/'
+
+axios(url)
+    .then(res => {
+        const html = res.data
+        const $ = cheerio.load(html)
+        const names = []
+        $('.product-title', html).each(() => {
+            const name = $(this).text()
+            names.push(name)
+        })
+        console.log(names)
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
+
+
